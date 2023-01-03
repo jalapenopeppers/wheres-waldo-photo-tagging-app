2f80dc81c6d9dc74890b82f7c9db55ffd3110dfa
Benjamin Q <benking3502@yahoo.com>
2023-01-03 10:57:14 -0500

    More project setup - Remove unnecessary project files - Setup project with correct Firebase account - Add levels data structure in local files and on firestore

--- wheres-waldo-photo-tagging-app/src/index.js
+++ wheres-waldo-photo-tagging-app/src/index.js
@@ -2,18 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBOF-8f9NOq8v-hIVZDorIIMKWRB0wgV5Q",
-  authDomain: "where-s-waldo-a40e3.firebaseapp.com",
-  projectId: "where-s-waldo-a40e3",
-  storageBucket: "where-s-waldo-a40e3.appspot.com",
-  messagingSenderId: "203974093335",
-  appId: "1:203974093335:web:28d28241df1e4f940d4a52"
+  apiKey: "AIzaSyAxQ3tiGuBwAceE5T14m8fojOfMRCuhZuI",
+  authDomain: "wheres-waldo-31bd7.firebaseapp.com",
+  projectId: "wheres-waldo-31bd7",
+  storageBucket: "wheres-waldo-31bd7.appspot.com",
+  messagingSenderId: "377940125644",
+  appId: "1:377940125644:web:fad96d395495fb4a8f1f69"
 };
 
 // Initialize Firebase
---
+++ wheres-waldo-photo-tagging-app/src/levels/level-1/level-1.js
@@ -0,0 +1,7 @@
+export const LevelObj = {
+  'level': 'Level 1',
+  'title': 'Desert Village',
+  'difficulty': 'Easy',
+  'description': 'Find the 3 characters in the desert village.',
+  'firebaseLevelID': 'level-1',
+};
