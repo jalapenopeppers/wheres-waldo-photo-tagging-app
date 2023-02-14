0b6498f47bc27f575297701732b28633cb2faaf1
Benjamin Q <benking3502@yahoo.com>
2023-02-13 22:43:16 -0500

    Add complete "about" page

--- wheres-waldo-photo-tagging-app/src/components/About.js
+++ wheres-waldo-photo-tagging-app/src/components/About.js
@@ -3,7 +3,28 @@ import './About.css';
 function About() {
   return (
     <div className="About">
-      <h1>About page</h1>
+      <h1 className="about-page-title">About</h1>
+      <p className="paragraph">
+        This web application was made as part of The Odin Project's curriculum
+        for full-stack web development. 
+      </p>
+      <p className="paragraph">
+        This website was made using ReactJS with React DOM Router and features cloud storage using Firestore, the 
+        Firebase cloud storage database. The cloud storage holds coordinates for 
+        each level's target characters and it also holds the leaderboards of each level.
+      </p>
+      <p className="paragraph">
+        The data for each level is stored as files in the site repository, but their structure
+        is simple so that anyone can easily create their own levels. All that is needed is
+        the JPG file of the level, the JPG files of the target characters, and the level
+        Javascript file which contains all the unique data pertaining to that level. Simply
+        adding a folder following the existing folder structure automatically adds the level
+        to the website, and the required cloud storage is automatically set up too.
+      </p>
+      <p className="paragraph">
+        For more information/instructions, 
+        visit the github repository <a href="https://github.com/jalapenopeppers/wheres-waldo-photo-tagging-app"> here.</a>
+      </p>
     </div>
   );
 }
